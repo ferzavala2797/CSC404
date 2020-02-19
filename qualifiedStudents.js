e3fc1694a9be19f4044b26db6d85a6ed7bac2da7
BrandonFonticoba <45527316+BrandonFonticoba@users.noreply.github.com>
2020-02-18 21:03:29 -0500

    Update qualifiedStudents.js

--- qualifiedStudents.js
+++ qualifiedStudents.js
@@ -1,14 +1,23 @@
 "use strict"
 
+//require gradeInput.js file
 const gradeInput = require('./gradeInput');
+
+//List of gpa scores from students.
 let gpa = gradeInput.averageScores;
+
+//List of students names.
 let names = gradeInput.nameList;
 
+//Array for Qualified Students
 let qualifiedStudents = new Array()
 
 for(var i = 0; i < gpa.length; i++)
 {
+    //Parse score from string to float.
     var gpaScore = parseFloat(gpa[i]);
+
+    //If gpa is greater than or equal to 2.5 add student name to list.
     if(gpaScore >= 2.5)
         qualifiedStudents.push(names[i]);
 }
